987b94a2aa1ac390eb30985d1591b078f7065e76
katty <a20160555@pucp.edu.pe>
2020-04-27 20:35:20 -0500

    commit katty editar borrar job

--- src/main/java/com/example/lab2/Controller/JobController.java
+++ src/main/java/com/example/lab2/Controller/JobController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/job")
@@ -36,7 +39,26 @@ public class JobController {
         return "redirect:/job/lista";
     }
 
-    @GetMapping("")
+    @GetMapping("/editar")
+    public String editarpersona(@RequestParam("id") String id, Model model){
+        Optional<Job> opt = jobRepository.findById(id);
+        if (opt.isPresent()){
+            Job job = opt.get();
+            model.addAttribute("job",job);
+            return "/job/editar";
+        }else {
+            return "redirect:/job/lista";
+        }
+    }
+
+    @GetMapping("/borrar")
+    public String borrarpersona(@RequestParam("id") String id){
+        Optional<Job> opt = jobRepository.findById(id);
+        if (opt.isPresent()) {
+            jobRepository.deleteById(id);
+        }
+        return "redirect:/job/lista";
+    }
 
 }
 
